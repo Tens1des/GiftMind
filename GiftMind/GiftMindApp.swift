86adf8567637def6bcfccb3fd03daddcc279e295
Tens1des <romankotov3213@gmail.com>
2025-09-27 12:50:25 +0300

    Initial Commit

---
+++ GiftMind/GiftMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftMindApp.swift
+//  GiftMind
+//
+//  Created by Рома Котов on 27.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct GiftMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
